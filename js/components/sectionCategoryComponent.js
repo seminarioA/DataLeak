6cbb8237e6c61fa327286dabacf13ddc55e9f298
ALEJANDRO VALENTINO SEMINARIO MEDINA <u22247454@utp.edu.pe>
2025-11-16 20:10:17 -0500

    [Update] improve formatting and readability in sectionCategoryComponent.js

--- js/components/sectionCategoryComponent.js
+++ js/components/sectionCategoryComponent.js
@@ -1,10 +1,12 @@
 function htmlSectionCategoryComponent(category, containerId) {
     let component = `
-
             <hr class="m-5 h-0.5 border-t-0 bg-neutral-100 dark:bg-white/10" />
             <section class="m-5">
-                <h2 class="text-white text-2xl">${category}</h2>
-                <div id="${containerId}" class="flex grid-cols-1 bg-gray-950 my-5 gap-5"></div>
+                <h2 class="text-white text-2xl">
+                    ${category}
+                </h2>
+                <div id="${containerId}" class="flex grid-cols-1 bg-gray-950 my-5 gap-5">
+                </div>
             </section>
 
         `
