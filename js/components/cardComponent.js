7700376236420794129c27360cdba2bef14a34b9
ALEJANDRO VALENTINO SEMINARIO MEDINA <u22247454@utp.edu.pe>
2025-11-16 21:42:35 -0500

    [Update] implement downloadBook function and update card component to use it

--- js/components/cardComponent.js
+++ js/components/cardComponent.js
@@ -1,20 +1,6 @@
-async function downloadBook(filePath) {
-    const { data, error } = await supabase.storage
-        .from("dataleake")
-        .createSignedUrl(filePath, 3600); // duración 1 hora
-
-    if (error) {
-        console.error("Error generando URL:", error);
-        return;
-    }
-
-    window.open(data.signedUrl, "_blank");
-}
-
-
 function htmlCardComponent(bg_color, url_image, book_title, authors_book, book_file_path) {
     let component = `
-        <a href="" onclick="downloadBook(${book_file_path})">
+    <a href="javascript:downloadBook('${book_file_path}');"> 
                 <div
                     class="max-w-60 rounded-3xl ${bg_color}-700 transition duration-75 hover:scale-102 hover:${bg_color}-800 h-full">
                     <img class="rounded-3xl p-1.5" src="${url_image}"
@@ -25,6 +11,8 @@ function htmlCardComponent(bg_color, url_image, book_title, authors_book, book_f
                         <p class="mb-2 text-base text-gray-300 line-clamp-1">${authors_book.join(', ')}</p>
                     </div>
                 </div>
-            </a>`
+    </a>
+            `
     return component;
 }
+
