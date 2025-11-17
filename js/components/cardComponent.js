5cce591d37205ac08947318ea9d79c4829ebf473
ALEJANDRO VALENTINO SEMINARIO MEDINA <u22247454@utp.edu.pe>
2025-11-16 21:10:21 -0500

    [Update] add function to generate signed URL for book downloads and update card component to use it

--- js/components/cardComponent.js
+++ js/components/cardComponent.js
@@ -1,6 +1,20 @@
-function htmlCardComponent(bg_color, url_image, book_title, authors_book) {
+async function downloadBook(filePath) {
+    const { data, error } = await supabase.storage
+        .from("dataleake")
+        .createSignedUrl(filePath, 3600); // duración 1 hora
+
+    if (error) {
+        console.error("Error generando URL:", error);
+        return;
+    }
+
+    window.open(data.signedUrl, "_blank");
+}
+
+
+function htmlCardComponent(bg_color, url_image, book_title, authors_book, book_file_path) {
     let component = `
-            <a href="#">
+        <a href="" onclick="downloadBook(${book_file_path})">
                 <div
                     class="max-w-60 rounded-3xl ${bg_color}-700 transition duration-75 hover:scale-102 hover:${bg_color}-800 h-full">
                     <img class="rounded-3xl p-1.5" src="${url_image}"
