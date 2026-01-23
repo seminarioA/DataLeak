973ddb0b814fe337df9c424fc68fb98d8953e71f
ALEJANDRO VALENTINO SEMINARIO MEDINA <u22247454@utp.edu.pe>
2026-01-23 11:17:37 -0500

    [Add] Implement footer component and load it in (index.html)

---
+++ js/components/footerComponent.js
@@ -0,0 +1,10 @@
+function htmlFooterComponent(){
+   let footer = `
+
+    <hr class="m-5 h-0.5 border-t-0 bg-neutral-100 dark:bg-white/10" />
+    <footer>
+    <p class="font-mono text-xl text-center text-white m-5">Made with 0% IA</p>
+    </footer>
+   `
+   return footer;
+}
