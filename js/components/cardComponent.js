029d9c2e96a0d6dab0377fcf630b982247d0c873
ALEJANDRO VALENTINO SEMINARIO MEDINA <u22247454@utp.edu.pe>
2026-02-07 14:39:17 -0500

    [Fix] Correct image class properties in htmlCardComponent for consistent styling

--- js/components/cardComponent.js
+++ js/components/cardComponent.js
@@ -3,7 +3,7 @@ function htmlCardComponent(bg_color, url_image_front, url_image_back, book_title
     <a href="javascript:downloadBook('${book_file_path}');"> 
         <div
             class="max-w-60 min-w-60 min-h-102.5 rounded-3xl ${bg_color}-700 transition duration-75 hover:scale-102 hover:${bg_color}-800 h-full">
-            <img class="rounded-3xl p-1.5 min-h-80" src="${url_image_front}"/>
+            <img class="w-full h-80 rounded-3xl p-1.5" src="${url_image_front}"/>
             
             <div class="px-3 py-1">
                 <div class="text-md mt-0 mb-0 font-bold text-white line-clamp-2">${book_title} 
